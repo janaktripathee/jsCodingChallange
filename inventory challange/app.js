48d7536cbb71cababb980e0321031482225b000f
janak tripathee <janaktripathee@gmail.com>
2021-06-16 22:36:05 -0700

    challange question added

---
+++ inventory challange/app.js
@@ -0,0 +1,18 @@
+//Develop an inventory application for a bookstore. 
+//Create a book class,which provides information about different books in store .
+//each book will have a title , author, ISBN, and keep track of the number of available copies.
+
+// check the availability of each book's if there are not any copies of the book left, the function should return
+//out of stock and if there are less than 10 copies the function should return low stock.
+//otherwise the function should return in stock.
+//also need function for selling a book which will take the number of copies sold and substract if from the total 
+//number of copies.
+//if no arguments is passed , then default the number of copies to sell to one.
+//also want a restock function which takes in a number of copies to restock and it to the total number of copies
+//if no arguments is passed, default the restock number to five.
+
+
+//use javascript class keywoard as well as a getter function for the availability method.
+
+
+
